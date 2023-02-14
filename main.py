b7ff10c72c39e13a41dd75f6e3151ffa9dfc92ac
HelenaAnna <125254498+HelenaAnna@users.noreply.github.com>
2023-02-14 20:45:35 +0000

    meg9

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            opening_brackets_stack.append(Bracket(next, i + 1))
+            opening_brackets_stack.append(Bracket(next, i))
             
             pass
 
@@ -31,7 +31,7 @@ def find_mismatch(text):
             opening_brackets_stack.pop()
             
     if opening_brackets_stack:
-        return opening_brackets_stack[0].position
+        return opening_brackets_stack[0].position +1
 
 
     return "Success"
