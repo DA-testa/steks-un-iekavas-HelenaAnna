6197d10d04c4472f2433d8e4acca811a3a7d7658
HelenaAnna <125254498+HelenaAnna@users.noreply.github.com>
2023-02-14 20:44:23 +0000

    meeginajums8

--- main.py
+++ main.py
@@ -41,7 +41,10 @@ def find_mismatch(text):
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    print(mismatch)
+    if mismatch == "Success"
+        print("Success")
+    else:
+        print(mismatch)
 
 
 if __name__ == "__main__":
