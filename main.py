86b600b90ee87d1acdb6b11bd8f83179428e2e9c
HelenaAnna <125254498+HelenaAnna@users.noreply.github.com>
2023-02-14 21:05:30 +0000

    meg13

--- main.py
+++ main.py
@@ -17,19 +17,17 @@ def find_mismatch(text):
             opening_brackets_stack.append(Bracket(next, i + 1))
             
 
-        if next in ")]}":
+        elif next in ")]}":
             # Process closing bracket, write your code here
             if not opening_brackets_stack:
                 return i + 1 
             
-            if not are_matching(opening_brackets_stack[-1].char,next):
-                return i + 1
-
             opening_bracket = opening_brackets_stack.pop()
-            
-            if not are_matching(opening_bracket.char, next):
 
+            if not are_matching(opening_bracket.char, next):
                 return i + 1
+
+            
 
     if opening_brackets_stack:
         return opening_brackets_stack[0].position
