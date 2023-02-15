96de69937d14c39ae4067af81c8b6d7d9bf68380
HelenaAnna <125254498+HelenaAnna@users.noreply.github.com>
2023-02-15 08:43:20 +0000

    t35

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
 
 
-
 def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
@@ -17,11 +16,11 @@ def find_mismatch(text):
 
         if next in ")]}":
             if not opening_brackets_stack:
-                return i + 1
+                return i
 
             top = opening_brackets_stack.pop()
             if not are_matching(top.char, next):
-                return i + 1
+                return i
 
     if opening_brackets_stack:
         return opening_brackets_stack[0].position
@@ -30,9 +29,16 @@ def find_mismatch(text):
 
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    print(mismatch)
+    text = input().strip()
+    if not all(bracket in "([{)]}" for bracket in text):
+        print("Invalid input format. Please enter a string containing only brackets.")
+    else:
+        mismatch = find_mismatch(text)
+        print(f"Expected: {mismatch}")
+        if mismatch == "Success":
+            print("Success")
+        else:
+            print(f"Actual: {mismatch+1}")
 
 if __name__ == "__main__":
     main()
