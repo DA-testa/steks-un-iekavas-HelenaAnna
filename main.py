6b9ade7aa47e5edbb683a6c30ff730e802aefbd9
HelenaAnna <125254498+HelenaAnna@users.noreply.github.com>
2023-02-15 08:26:04 +0000

    t29

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 
 from collections import namedtuple
 
-Bracket = namedtuple("Bracket", ["char", "position"])
+import sys
 
+Bracket = namedtuple("Bracket", ["char", "position"])
 
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
-
 
 def find_mismatch(text):
     opening_brackets_stack = []
@@ -29,13 +29,26 @@ def find_mismatch(text):
         return "Success"
 
 def main():
+    mode = input("Choose mode (F for file or I for input): ")
+    if mode.upper() == "F":
+        filename = input("Enter filename: ")
+        try:
+            with open(filename, "r") as file:
+                text = file.read().strip()
+        except FileNotFoundError:
+            print("File not found.")
+            sys.exit(1)
+    elif mode.upper() == "I":
+        text = input("Enter brackets: ")
+    else:
+        print("Invalid mode choice.")
+        sys.exit(1)
 
     mismatch = find_mismatch(text)
     if mismatch == "Success":
         print("Success")
     else:
         print(mismatch)
-
 
 if __name__ == "__main__":
     main()
